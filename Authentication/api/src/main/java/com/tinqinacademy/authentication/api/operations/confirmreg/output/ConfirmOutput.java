028e3e083051948c0e388da7d414800cb2844303
RostislavKirilov <71329390+RostislavKirilov@users.noreply.github.com>
2024-08-11 20:59:51 +0300

    [HSM-5] Added new operations (#6)

---
+++ Authentication/api/src/main/java/com/tinqinacademy/authentication/api/operations/confirmreg/output/ConfirmOutput.java
@@ -0,0 +1,13 @@
+package com.tinqinacademy.authentication.api.operations.confirmreg.output;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class ConfirmOutput {
+    private String message;
+}
